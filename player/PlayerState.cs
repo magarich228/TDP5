5eac09d71165dc401adbcd208aa4ed676ac54c6d
magarich228 <kirillgroshev52@gmail.com>
2024-02-18 21:09:44 +0300

    Добавлены все анимации. Удалены лишние файлы.

--- player/PlayerState.cs
+++ player/PlayerState.cs
@@ -2,12 +2,12 @@ namespace Tdp5.player;
 
 public enum PlayerState : int
 {
-    Stand = 0,
-    Run = 1,
-    Jump = 2,
-    Sit = 3,
-    SitWalk = 4,
-    Lie = 5,
-    Creep = 6,
-    Die = 7
+	Stand = 0,
+	Run = 1,
+	Jump = 2,
+	Sit = 3,
+	SitWalk = 4,
+	Lie = 5,
+	Creep = 6,
+	Die = 7
 }
