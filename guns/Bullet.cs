45b304353165a79f42a9f7e14415b3f26f8c3c60
magarich228 <kirillgroshev52@gmail.com>
2025-01-06 13:55:54 +0300

    testmap2 added.

--- guns/Bullet.cs
+++ guns/Bullet.cs
@@ -1,23 +1,17 @@
 using System;
 using Godot;
 
-public partial class Bullet : RigidBody2D
+public partial class Bullet : Node2D
 {
 	// Godot public properties.
 	// ReSharper disable MemberCanBePrivate.Global
 	public const float Speed = 1800.0f;
 	// ReSharper restore MemberCanBePrivate.Global
-	
-	public override void _Ready()
-	{
-		ContactMonitor = true;
-		MaxContactsReported = 10;
-	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		MoveAndCollide(new Vector2((float)Speed * (float)delta, 0)
-			.Rotated(GlobalRotation + Mathf.DegToRad(-90f)));
+		Position += new Vector2((float)Speed * (float)delta, 0)
+			.Rotated(GlobalRotation + Mathf.DegToRad(-90f));
 		
 		if (Position.X > GetViewportRect().Size.X)
 		{
@@ -28,15 +22,6 @@ public partial class Bullet : RigidBody2D
 		{
 			QueueFree();
 		}
-
-		var collisions = GetCollidingBodies();
-
-		if (collisions.Count > 0)
-		{
-			QueueFree();
-		}
-		
-		Console.WriteLine(collisions.Count);
 		
 		base._PhysicsProcess(delta);
 	}
